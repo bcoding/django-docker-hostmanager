82754c5defdc28b25474c38ce2793201dbd98f60
André Böhlke <ab@bcoding.de>
2015-04-18 01:04:12 +0200

    create skeleton project and add some documentation

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # django-docker-hostmanager
 This django app is part of a homegrown docker host management system.
+
+## Contents
+
+* `docker_hostmanager`
+contains a simple django project containing the REST API and a Django admin
+* `docker_hostmanager.api`:
+This contains a django app which knows how to manage containers on the docker host(s).
+* `docker_hostmanager.rest`:
+This contains a django app which makes `docker_hostmanager.api` accessible as REST API 
+
+## API
+
+POST /container_group - Neue Container-Gruppe definieren
+```json
+{
+    "name": "openslides-1.7",
+    "image": "OpenSlides/openslides-1.7",
+    "static_containers": {
+        "static_files": {
+            "volumes": ["/static"],
+            "command": "pyhton manage.py collectstatic --noinput",
+            "environment": {
+                "STATIC_ROOT": "/static",
+                ...
+            }
+        }
+    },
+    "web_resources": [
+        {
+            "domain": "static.openslides.de",
+            "path": "1.7",
+            "volume": "static_files->/static"
+        }
+    ]
+}
+```
+
+POST /containers - Neuen Container starten
+```json
+{
+    "group": "openslides-1.7",
+    "vhost": {
+        "protocol": "https",
+        "domains": ["openslides.acme.net", "acme.openslides.de"],
+        "path": "/event1",
+	...
+    },
+    "web_resources": [
+        {
+            "protocol": "%(vhost.protocol)",
+            "domains": ["openslides.acme.net", "acme.openslides.de"],
+            "path": "/media",
+            "volume": "fce96b8d030a27046da05207d0a52f5109958c1affc2965a338c62b775259f49->/media"
+        }
+    ],
+    "container": {
+        "volumes_from": ["fce96b8d030a27046da05207d0a52f5109958c1affc2965a338c62b775259f49"],
+        "environment": {
+            "DATABASE_URL": "sqlite:///data/openslides.sqlite",
+            "STATIC_URL": "%(vhost.protocol)sstatic.openslides.de/1.7",
+            "MEDIA_ROOT": "/media"
+            ...
+        }
+    }
+```
---
+++ docker_hostmanager/settings.py
@@ -0,0 +1,76 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'sj0q9n1_h=b8my#6-n^r=l5=hgekx4gwrl1nmaoox^-_%6=%qj')
+
+DEBUG = True
+
+if not DEBUG and not 'SECRET_KEY' in os.environ:
+    raise Exception('production environments must have it\'s own SECRET_KEY')
+
+ALLOWED_HOSTS = []
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'docker_hostmanager.api',
+    'docker_hostmanager.rest',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'docker_hostmanager.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'docker_hostmanager.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'hostmanager.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '.'
